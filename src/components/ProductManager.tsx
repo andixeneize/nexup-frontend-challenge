c5f91fef4062a8c67ec3939878132c8360b366bb
andixeneize <ae.sanchez89@gmail.com>
2026-01-14 17:09:10 -0300

    feat: add mock products data with async API simulation

--- src/components/ProductManager.tsx
+++ src/components/ProductManager.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ProductList } from './ProductList';
 import { CategoryFilter } from './CategoryFilter';
+import { getProductList } from '../api/products';
 
 export const ProductManager: React.FC = () => {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getProductList();
+      console.log('Products fetched from API:', products);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <CategoryFilter />
